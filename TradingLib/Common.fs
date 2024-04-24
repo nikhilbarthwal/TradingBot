0e2093508b19710c06df09a85f94dbe027c35b0c
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2024-04-24 08:43:57 -0700

    Adding MAtrix

--- TradingLib/Common.fs
+++ TradingLib/Common.fs
@@ -33,6 +33,32 @@ module Vector =
         let init = (f1 0) * (f2 0) in (List.fold f init [1 .. l - 1])
 
 
+type Matrix<'V, 'T when 'T :> Array<'V>> private(rows: int, f: int -> 'T) =
+
+    let data:Array<'T> = Array.Create rows f
+    member this.Item with get(i: int):'T = data[i]
+    member this.Row i: int -> 'V = fun j -> data[i][j]
+    member this.Column j: int -> 'V = fun i -> data[i][j]
+    member this.Rows: int = data.Size
+    member this.Columns: int = this[0].Size
+
+    static member Var<'N>((rows, columns): int * int) (gen: int * int -> 'N) =
+        let row i = Array.Buffer(columns, fun j -> gen (i, j)) in Matrix(rows, row)
+
+    static member Const<'N>((rows, columns): int * int) (gen: int * int -> 'N) =
+        let row i = Array.Create columns (fun j -> gen (i, j)) in Matrix(rows, row)
+
+    static member Vandermonde(order: int) (inp: Array<Fraction>) =
+        let row (_: int) = Array.Buffer(inp.Size, fun _ -> Fraction(1))
+        let data = Array.Create (order + 1) row
+        let write (i: int) (j:int) = data[i-1][j] * inp[j]
+        for i in 1 .. order do data[i].Overwrite(write i)
+        Matrix(data.Size, fun i -> data[i] :> Array<Fraction>)
+
+type MatrixConst<'T> = Matrix<'T, Array<'T>>
+type MatrixVar<'T> = Matrix<'T, Array.Buffer<'T>>
+
+
 type Node<'T, 'R> =
     abstract Size: int
     abstract Combine: ('T * 'R * 'R) -> 'R
