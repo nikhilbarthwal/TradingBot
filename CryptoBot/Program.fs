e2ce77caac6bf492bf43f9d1520750f2be8f8922
Nikhil Barthwal <nikhilbarthwal@hotmail.com>
2024-04-27 00:13:33 -0700

    .

--- CryptoBot/Program.fs
+++ CryptoBot/Program.fs
@@ -9,11 +9,12 @@ let tickers: Ticker list = [ Crypto("BTC") ] // ; Crypto("BTC") ]
 let source: Source = Gemini.Source {|
     Tickers = tickers
     Size = size
-    Buffer = Buffer.Linear(5L, 5)
+    Buffer = Buffer.Linear(1L, 5)
     AskBidDifference = 0.15
     Timeout = 5 |}
 
 for i = 1 to 100 do
+    printfn $" ***** {i} *****"
     let b = source.Data[Crypto("BTC")].Get(data)
-    if b then (for d in data do printfn "%A" d) else printfn "SKIP"
+    if b then (for d in data do printfn "Output -> %A" d) else printfn "SKIP"
     Utils.Wait(5)
--- TradingLib/Buffer.fs
+++ TradingLib/Buffer.fs
@@ -1,4 +1,7 @@
 namespace TradingLib
+#if DEBUG
+open System.Diagnostics
+#endif
 
 
 type Buffer = abstract member Ingest: Bar * (Bar -> unit) -> bool
@@ -8,13 +11,11 @@ module Buffer =
     type private Bucket() =
         let mutable data = Bar()
         let mutable count = 0
-        let mutable valid = false
         let merge (b: Bar) =
             let pF = float count
             let tF = float <| count + 1
             let pL = int64 count
             let tL = int64 <| count + 1
-            count <- count + 1
             let avgFloat (p, c) = (pF * p + c) / tF
             let avgLong (p, c) = (pL * p + c) / tL
             Bar <| {| Open   = avgFloat(data.Open, b.Open)
@@ -24,89 +25,89 @@ module Buffer =
                       Time   = avgLong(data.Epoch, b.Epoch)
                       Volume = avgLong(data.Volume, b.Volume) |}
 
-        member this.Data = assert valid ; data
-        // member this.Add(x: Bar) = if data.Valid then data <- merge x else data <- x
-        member this.Add(x: Bar) =
-            if valid then
-                let t = data
-                data <- merge x
-                Log.Debug("Merge", $"{count} {x}: {t} -> {data}")
-            else
-                Log.Debug("Merge", $"{count} {x}: Initial")
-                data <- x
-                valid <- true
-
-        member this.Reset() = data <-Bar()
+        member this.Data = Debug.Assert(count > 0, $"count = {count}") ; assert (count > 0) ; data
+        member this.Reset() = data <-Bar() ; count <- 0
         member this.Count = count
+        member this.Add(x: Bar) =
+            if count > 0 then (data <- merge x) else (data <- x)
+            count <- count + 1
+
 
     type private Buckets(size: int) =
         let mutable pos = 0
         let buckets = [| for _ in [1 .. size] do Bucket() |]
         let index k = (pos + k) % size
 
+        member this.Debug(tag: string) =
+            Log.Debug("DEBUG", $" ***** {tag} : pos = {pos} *****")
+            for i in [1 .. size] do Log.Debug("DEBUG", $" {i}: {buckets[i-1].Count}")
+
         member this.Item with get(k: int) = buckets[index(k)]
-        member this.Previous() = buckets[0].Data.Epoch
-        member this.Shift(k) = pos <- (pos + k) % size
+        member this.Previous() = buckets[pos].Data.Epoch
+        member this.Shift(k) =
+            this.Debug($"Before Shift {k}")
+            assert (k > 0)
+            for i in [1 .. k] do buckets[index <| i-1].Reset()
+            pos <- (pos + k) % size
+            this.Debug($"After Shift {k}")
+
         member this.Reset() =
             pos <- 0 ; for i in [0 .. size - 1] do buckets[i].Reset()
 
-    type private LinearBuffer(interval: time, size: int) =
+    type Linear(interval: time, size: int) =
 
         let buckets = Buckets(size)
         let mutable previous: time = 0L
-        let floor (t:time) = t - (t % interval)
+        let floor (t:time) = t - (t % (int64 interval))
 
-        (*
-        let extrapolate (diff: int) (output: Bar -> unit) (i: int): unit =
-            let t = previous + interval * (int64 i)
-            let p, c = buckets[0].Data, buckets[diff].Data
-            let dt = c.Epoch - p.Epoch
+        let extrapolate (diff: int) (output: Bar -> unit): unit =
+            Log.Debug("Extrapolate", $"Extrapolate diff = {diff}")
+#if DEBUG
+            Debug.Assert(diff > 0)
+            Debug.Assert(buckets[0].Count > 0, "First count is empty")
+            Debug.Assert(buckets[diff].Count > 0, "Last count is empty")
+            if diff > 1 then
+                for k in [ 1 .. (diff - 1)] do
+                    Debug.Assert(buckets[k].Count = 0, $"Bucket @ {k} is not empty")
+#endif
+            let prev = buckets[0].Data
+            let curr = buckets[diff].Data
+            let f (p: float, c: float) (r: float) = p + (c - p) * r
 
-            let extrapolateF (prev: float, curr: float): float =
-                prev + ((curr - prev) * (float <| t - p.Epoch)) / (float <| dt)
-
-            let extrapolateL (prev: int64, curr: int64): int64 =
-                prev + ((curr - prev) * (t - p.Epoch)) / dt
-
-            output <| Bar({| Open   = extrapolateF(p.Open, c.Open)
-                             Close  = extrapolateF(p.Close, c.Close)
-                             High   = extrapolateF(p.High, c.High)
-                             Low    = extrapolateF(p.Low, c.Low)
-                             Time   = extrapolateL(p.Epoch, c.Epoch)
-                             Volume = extrapolateL(p.Volume, c.Volume) |})
-        *)
+            for k in [1 .. diff] do
+                let r = (float <| k-1) / (float diff)
+                let dv = int64 <| (float <| (curr.Volume - prev.Volume)) * r
+                // output <| Bar({| Open   = f(prev.Open, curr.Open) r
+                let b =   Bar({| Open   = f(prev.Open, curr.Open) r
+                                 Close  = f(prev.Close, curr.Close) r
+                                 High   = f(prev.High, curr.High) r
+                                 Low    = f(prev.Low, curr.Low) r
+                                 Time   = previous + interval * (int64 <| k - 1)
+                                 Volume = prev.Volume + dv |})
+                Log.Debug("Extrapolate", $"Inserting {b}")
+                output b
 
         interface Buffer with
             member this.Ingest(input: Bar, output: Bar -> unit): bool =
-                Log.Debug("Input", input.ToString())
                 let current = floor input.Epoch
+                Log.Debug("Input", $" Previous = {previous} / Current = {current} -> {input}")
+                Log.Debug("Debug", $"buckets[0].Count = {buckets[0].Count}")
+                buckets.Debug(" -----------------------")
                 if buckets[0].Count = 0 then
                     buckets[0].Add input
                     if input.Epoch % interval = 0 then output(input)
-                    previous <- current
-                    true
+                    Log.Debug("Diff", $"Initial {input}")
+                    previous <- current ; true
                 else
+                    Log.Debug("Debug", $"That input.Epoch = {input.Epoch}")
+                    Log.Debug("Debug", $"That {input.Epoch} >= {buckets.Previous()}")
                     assert (input.Epoch >= buckets.Previous())
+                    Log.Debug("Debug", "That")
                     let diff = int <| (current - previous) / interval
-
-                (*
-                        if diff >= size then
-                            buckets.Reset() ; false
-                        else
-                            buckets[diff].Add input
-                            for i in [1 .. diff] do (output <| buckets[i-1].Data)
-                            // (extrapolate diff output i)
-                            buckets.Shift(diff) ; true
-                *)
-
-                    let temp = buckets.Previous()
-                    if input.Epoch < temp then
-                        Log.Debug("Error", $"Current = {input.Epoch} / Previous = {temp} / Floor = {current} / Diff = {diff}")
-                    else
-                        Log.Debug("Ingest", $"{input.Epoch} / Previous = {temp} / Floor = {current} / Diff = {diff}")
-                    output <| input
-                    buckets[0].Add input
-                    true
-
-
-    let Linear z: Buffer = LinearBuffer z :> Buffer
+                    Log.Debug("Diff", $"Diff = {diff} -> {input}")
+                    if diff >= size then (buckets.Reset() ; false) else
+                        buckets[diff].Add input
+                        if diff > 0 then
+                            extrapolate diff output
+                            buckets.Shift(diff)
+                        previous <- current ; true
